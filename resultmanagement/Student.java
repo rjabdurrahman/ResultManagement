0dd60071d4011aa70aa1d4ffc042b0412d003c28
rjabdurrahman <md.rahman0296@gmail.com>
2017-11-26 20:42:09 +0600

    Student List Updated

---
+++ resultmanagement/Student.java
@@ -0,0 +1,19 @@
+package resultmanagement;
+
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+public class Student{
+    private SimpleIntegerProperty roll;
+    private SimpleStringProperty name;
+    public Student(Integer roll, String name){
+        this.roll = new SimpleIntegerProperty(roll);
+        this.name = new SimpleStringProperty(name);
+    }
+    public Integer getRoll(){
+        return roll.get();
+    }
+    public String getName(){
+        return name.get();
+    }
+}
